3dedf19141e30d9809f9b523ced680007474dce2
Chris Jazinski <chris@jazinski.com>
2014-08-04 22:37:01 -0500

    Added compass

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,18 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
-var browserify =  require('gulp-browserify');
+var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 
 //Easier to read
 var coffeeSources = ['components/coffee/tagline.coffee']; //can use * wildcard
 var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare: true})
-			.on('error', gutil.log))
+		.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'));
 });
 
@@ -21,4 +23,16 @@ gulp.task('js', function() {
 		.pipe(concat('scripts.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
+
 });
