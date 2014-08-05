f7ec79e609cd4d5821787c81166d0f4d543d03a9
Chris Jazinski <chris@jazinski.com>
2014-08-04 23:23:48 -0500

    Now with env

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,24 @@ var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var compass = require('gulp-compass');
 var connect = require('gulp-connect');
+//To get the 2 operating modes
+var env, coffeeSources, jsSources, sassSources, htmlSources, outputDir, sassStyle;
+env = process.env.NODE_ENV || 'development';
 
-//Easier to read
-var coffeeSources = ['components/coffee/tagline.coffee']; //can use * wildcard
-var jsSources = ['components/scripts/*.js'];
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
+if (env === 'development') {
+	outputDir ='builds/development/';
+	sassStyle = 'expanded';
+} else {
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/tagline.coffee']; //can use * wildcard
+jsSources = ['components/scripts/*.js'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+
+outputDir;
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -24,21 +36,21 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch'], function() {
+gulp.task('default', ['coffee', 'html', 'js', 'compass', 'connect', 'watch'], function() {
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'));
+		.pipe(gulp.dest(outputDir + 'js'));
 });
 
 //Starts a server
 gulp.task('connect', function() {
 	connect.server({
-		root: 'builds/development/',
+		root: outputDir,
 		livereload: true
 	});
 });
@@ -60,11 +72,11 @@ gulp.task('compass', function() {
 	gulp.src(sassSources)
 		.pipe(compass({
 			sass: 'components/sass',
-			image: 'builds/development/images',
-			style: 'expanded'
+			image: outputDir + 'images',
+			style: sassStyle
 		}))
 		.on('error', gutil.log)
-		.pipe(gulp.dest('builds/development/css'))
+		.pipe(gulp.dest(outputDir + 'css'))
 		.pipe(connect.reload())
 
 });
