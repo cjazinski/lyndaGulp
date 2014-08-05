0a420ec232a41c255d102788add07e20ffd1b57e
Chris Jazinski <chris@jazinski.com>
2014-08-04 22:22:36 -0500

    concat JS

---
+++ builds/development/js/scripts.js
@@ -0,0 +1,7 @@
+var fill;
+
+(fill = function(item) {
+  return $('.tagline').append("" + item);
+})('It is what it is');
+
+fill;
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
 
 //Easier to read
 var coffeeSources = ['components/coffee/tagline.coffee']; //can use * wildcard
+var jsSources = ['components/scripts/*.js'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare: true})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('builds/development/js'));
 });
